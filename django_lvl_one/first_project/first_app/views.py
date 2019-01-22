b3db4334f65b078e13316cdd43056a29e604cc53
Anastasiya <37482238+ovinnikova@users.noreply.github.com>
2019-01-23 00:14:44 +0300

    Add files via upload

--- django_lvl_one/first_project/first_app/views.py
+++ django_lvl_one/first_project/first_app/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-# Each view is going to have at least one argument.
-# Each view must return an HTTP Response Obj.
+# def index(request):
+#     return HttpResponse("Hello World!")
 
 def index(request):
-    return HttpResponse("Hello World")
+    my_dict = {'insert_me':"Hello I am from views.py!"}
+    return render(request,'first_app/index.html',context=my_dict)
