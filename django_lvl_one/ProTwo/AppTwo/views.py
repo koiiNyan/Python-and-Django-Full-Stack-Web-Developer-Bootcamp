a5473b03c5eb07545b6441fe629a5f352d14c8f3
Anastasiya <37482238+ovinnikova@users.noreply.github.com>
2019-01-23 17:23:51 +0300

    Add files via upload

--- django_lvl_one/ProTwo/AppTwo/views.py
+++ django_lvl_one/ProTwo/AppTwo/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<em>My Second App</em>")
+
+def help(request):
+    help_dict = {'hey': 'Help Page'}
+    return render(request, 'AppTwo/help.html', context=help_dict)
