255444dc66915996375ad7059acb268e98fb6507
Anastasiya <anovinnikova@yandex.ru>
2019-01-29 01:19:12 +0300

    Urls fixes

--- advanced_cbvs/adv_cbv/adv_cbv/urls.py
+++ advanced_cbvs/adv_cbv/adv_cbv/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 from basic_app import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view()),
+    path('', views.IndexView.as_view(),name='index'),
     path('admin/', admin.site.urls),
     path('basic_app/', include('basic_app.urls',namespace='basic_app'))
 ]
